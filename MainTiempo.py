5d37face30aed7e624412a23ae14800c17237de1
Juan Bregonzi <122122626+Juancho599@users.noreply.github.com>
2026-01-02 23:03:55 -0300

    Refactorización MainTiempo.py
    Refactorice: cargar la API key de OpenWeather desde variables de entorno
    
    Se eliminó la API key hardcodeada y se agregó soporte para configuración con un archivo .env, mejorando la seguridad y la facilidad de uso del proyecto.

--- MainTiempo.py
+++ MainTiempo.py
@@ -25,6 +25,16 @@ def obtener_clima(ciudad, api_key):
             print("Tu API key no es válida o expiró.")
 
 if __name__ == "__main__":
-    api_key = "TU_API_KEY_AQUÍ"  # ← Reemplaza esto con tu clave real
+    load_dotenv()
+
+    api_key = os.getenv("API_KEY")
+
+    if not api_key:
+        raise RuntimeError(
+            "API_KEY no encontrada. "
+            "Creá un archivo .env con la variable API_KEY"
+        )
+
     ciudad = input("Ingrese el nombre de una ciudad: ")
     obtener_clima(ciudad, api_key)
+
